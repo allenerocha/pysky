c347c6bfb923c5641da515edd1b758e8f8472c71
allen <allenerocha@pm.me>
2020-03-05 12:39:33 -0600

    Added the ability to add text to images

--- pysky.py
+++ pysky.py
@@ -5,6 +5,7 @@ import io
 import base64
 import PIL.Image
 import utils.skyview
+import slideshow.image_manipulation
 
 
 def main():
@@ -17,13 +18,21 @@ def main():
     :return:
     """
 
+    # todo compare date with previous run date
+    # todo check endpoints
+    # todo try-catch for successful server connection
+    # todo download brightness from simbad if successfully connected
+    # todo download image from skyview/nasa site if successfully connected
+    # todo overlay celesital statistics on the image
+    # todo add overlayed image to slideshow queue
+    # todo play slideshow
+
     utils.skyview.get_img(sys.argv[1], int(sys.argv[2]),
                           int(sys.argv[3]), float(sys.argv[4]), sys.argv[5])
     cache_file = json.loads(open("cache/data", "r").read())
     decoded_img = base64.b64decode(cache_file[sys.argv[1]]['image']['base64'][1:-1])
-    img = PIL.Image.open(io.BytesIO(decoded_img))
-    img.show()
-    print(cache_file[sys.argv[1]]['brightness'])
+    slideshow.image_manipulation.add_text(PIL.Image.open(io.BytesIO(decoded_img)), [f"Name: {sys.argv[1]}", f"Brightness: {cache_file[sys.argv[1]]['brightness']}"])
+
 
 
 if __name__ == '__main__':
--- slideshow/image_manipulation.py
+++ slideshow/image_manipulation.py
@@ -1,12 +1,24 @@
 """This module will be used to overlay information of the
 celestial body over image of the celestial body using PIL"""
 import PIL.Image
+import PIL.ImageDraw
+import PIL.ImageFont
 
-
-def add_text(*args: str):
+def add_text(img: object, overlay_text: list) -> object:
     """
     This adds text to the image
-    :param args: List of text to overlay on the image
+    :img: Image file to overlay the text
+    :overlay_text: List of text to overlay on the image
     :return: None
     """
-    pass
+
+    """
+    overlay_test -> [name, brightess]
+    """
+
+
+    _, img_h = img.size
+
+    overlayed = PIL.ImageDraw.Draw(img, mode='RGBA')
+    overlayed.multiline_text(xy=(10,int(img_h*0.8)), text="\n".join(overlay_text), fill=(255, 255, 255, 150), font=PIL.ImageFont.truetype("aakar-medium.ttf", 16), stroke_width=1, stroke_fill=(0, 0, 0, 100), spacing=1, align="left")
+    img.show()
