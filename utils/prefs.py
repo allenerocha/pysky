1db9ad4d2339ea90b31f7b8517dea3b68d8aa0e6
allen <allenerocha@pm.me>
2020-03-09 15:12:45 -0500

    Added documentation

--- utils/prefs.py
+++ utils/prefs.py
@@ -1,10 +1,9 @@
+"""This module is called at application launch and checks for user
+preferences and applies the cache rules to the cache file and saves it"""
 import os.path
 import sys
+import json
 
-# todo check for prefs file
-# todo if doesn't exist, make one
-# todo prefs_check() -> read_prefs() -> cache_rules() -> cache_clean()
-#                    -> guided_prefs_creation()
 
 def check_prefs():
     """
@@ -20,7 +19,10 @@ def create_prefs():
     """
     This module is used to create prefs file
     """
-    print('true')
+    # todo open dictionary of possible options
+    # todo display options
+    # todo user selects options
+    # todo dump json file to data/prefs
     sys.exit()
     pass
 
@@ -29,6 +31,11 @@ def clean_cache():
     """
     Reads prefs files and calls the prune function to remove certain keys
     """
+    # todo load data/prefs
+    # todo check cache rules
+    # todo iterate through the cache and apply rules
+    # todo call prune
+    # todo dump cache json to data/cache
     pass
 
 
@@ -36,6 +43,7 @@ def prune():
     """
     Removes a given key from the cache
     """
+    # todo pop/delete the key in the passed dictoionary
     pass
 
 
