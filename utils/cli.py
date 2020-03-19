a4903292260b5accfa2d24b3a77bb5abaf5de7c5
allen <allenerocha@pm.me>
2020-03-19 13:43:47 -0500

    Initial commit

---
+++ utils/cli.py
@@ -0,0 +1,15 @@
+"""This module is called after utils/prefs and parses the passed CLI options"""
+
+import time
+
+
+def cli_parse(*args) -> list:
+    """
+    POSITION 0 & 2 (DATE)
+    POSITION 1 & 3 (TIME)
+    FORMATS:
+        DATE:   YEAR-DAY-MON    (DIGITS)
+        TIME:   HOUR:MINUTE
+    """"
+
+
