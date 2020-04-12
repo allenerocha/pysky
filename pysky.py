34542173c69bd18aaa7825159ac6ead3481098e8
allen <allenerocha@pm.me>
2020-04-12 12:35:16 -0500

    Fixed formatting

--- pysky.py
+++ pysky.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
+# encoding: utf-8
 import json
-import time
-import sys
 import os.path
-import utils.skyview
-import utils.astro_info
-import utils.objectfilter
-import utils.image_manipulation
-import utils.prefs
-import utils.cli
-import utils.check_sky
-import astroplan
+import sys
+import time
+
 import astropy
+
+import astroplan
+import utils.astro_info
+import utils.check_sky
+import utils.cli
+import utils.image_manipulation
+import utils.objectfilter
+import utils.prefs
+import utils.skyview
 
 
 def main():
@@ -19,16 +22,29 @@ def main():
     """
     utils.prefs.check_integrity()
 
-    viewing_time_range = utils.cli.parse(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    viewing_time_range = utils.cli.parse(
+        sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    )
 
-    # utils.prefs.clean_cache()
+    utils.prefs.clean_cache()
 
-    earth_loc = astroplan.Observer(location=astropy.coordinates.EarthLocation.from_geodetic(-87.8791*astropy.units.deg, 42.6499*astropy.units.deg, height=204*astropy.units.m), name="Schoolyard Observatory", timezone="US/Central")
+    sys.exit()
 
-    celestial_objs = utils.objectfilter.emphemeries_filter('venus', 'polaris', 'neptune', 'vega', 'saturn', 'deneb', 'sirius', 'capella')
+    earth_loc = astroplan.Observer(
+        location=astropy.coordinates.EarthLocation.from_geodetic(
+            -87.8791 * astropy.units.deg,
+            42.6499 * astropy.units.deg,
+            height=204 * astropy.units.m,
+        ),
+        name="Schoolyard Observatory",
+        timezone="US/Central",
+    )
+
+    celestial_objs = utils.objectfilter.emphemeries_filter(
+        "venus", "polaris", "neptune", "vega", "saturn", "deneb", "sirius", "capella"
+    )
     STARS = celestial_objs[0]
     EPHEMERIES_BODIES = celestial_objs[1]
-
 
     db_calls(STARS)
 
@@ -45,7 +61,7 @@ def main():
     # Iterate through the ephemeries to add information
     for body in EPHEMERIES_BODIES:
         COORDS = utils.astro_info.get_info(body)
-        cache_file  = get_ephemeries_info(EPHEMERIES_BODIES, cache_file)
+        cache_file = get_ephemeries_info(EPHEMERIES_BODIES, cache_file)
     # Dump cache file
     with open("data/cache", "w") as json_out:
         json.dump(cache_file, json_out, indent=4, sort_keys=True)
@@ -56,7 +72,7 @@ def db_calls(celestial_objs):
     for celestial_obj in celestial_objs:
         # Call to download images
         utils.skyview.get_img(celestial_obj, 480, 480, 3.5, "Linear")
-        #cache_file = json.loads(open("data/cache", "r").read())
+        # cache_file = json.loads(open("data/cache", "r").read())
         utils.prefs.clean_cache()
 
 
@@ -66,15 +82,16 @@ def get_ephemeries_info(bodies: list, cache_file: dict) -> dict:
         COORDS = utils.astro_info.get_info(body)
         cache_file[f"{body}"] = {}
         cache_file[f"{body}"]["type"] = "planet"
-        cache_file[f"{body}"]["created"] = time.strftime("%Y-%d-%m %H:%M", time.gmtime())
-        cache_file[f"{body}"]["coordinates"] = {    # Right acension
-                                                    "ra": [str(COORDS[0]), str(COORDS[1]), str(COORDS[2])],
-                                                    "dec": str(COORDS[3]),
-                                                    "cartesian": [str(COORDS[5]), str(COORDS[6]), str(COORDS[7])]
-                                                }
+        cache_file[f"{body}"]["created"] = time.strftime(
+            "%Y-%d-%m %H:%M", time.gmtime()
+        )
+        cache_file[f"{body}"]["coordinates"] = {  # Right acension
+            "ra": [str(COORDS[0]), str(COORDS[1]), str(COORDS[2])],
+            "dec": str(COORDS[3]),
+            "cartesian": [str(COORDS[5]), str(COORDS[6]), str(COORDS[7])],
+        }
     return cache_file
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
