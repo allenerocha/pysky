ae7a8b724768b5363d9fd40833d312d87f7fe21e
allen <allenerocha@pm.me>
2020-04-16 11:09:38 -0500

    Skeletal framework for future tests

---
+++ tests/test_pysky.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""Tests for `pysky` package."""
+
+
+import unittest
+
+from pysky import __main__
+
+
+class TestPysky(unittest.TestCase):
+    """Tests for `pysky` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
