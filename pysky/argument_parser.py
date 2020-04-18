83e15d691df13ff3bf05a73a7c5d6ccd4f249610
allen <allenerocha@pm.me>
2020-04-18 10:33:42 -0500

    Cleaned structure and added type and value checking

--- pysky/argument_parser.py
+++ pysky/argument_parser.py
@@ -16,12 +16,32 @@ def cli_parse(root_dir: str, cli_args: list) -> list:
         DATE:   YEAR-DAY-MON    (DIGITS)
         TIME:   HOUR:MINUTE     (DIGITS)
     """
+
+    if not isinstance(root_dir, str) or len(root_dir) < 1:
+        root_dir = Path(__file__).parent
+
+    if not isinstance(cli_args, list):
+        print_help()
+        raise TypeError(
+            f"Value passed to cli_parse must be a list not {type(cli_args)}."
+        )
+
+    if not len(cli_args) == 4:
+        print_help()
+        raise IndexError(f"Argument for cli_parse must be size 4 not {len(cli_args)}.")
+
+    for arg in cli_args:
+        if not isinstance(arg, str):
+            print_help()
+            raise TypeError(f"{arg} should be a string type not {type(arg)}.")
+        if len(arg) < 1:
+            raise IndexError(f"Arguments for cli_parse cannot be empty.")
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
         handlers=[logging.FileHandler(f"{root_dir}/data/log"), logging.StreamHandler()],
     )
-
     try:
         info("Parsing cli input...")
         year_one = int(cli_args[0].split("-")[0])
@@ -51,20 +71,18 @@ def cli_parse(root_dir: str, cli_args: list) -> list:
             hour=hour_two,
             minute=minute_two,
         )
-
-        START_TIME = astropy.time.Time(datetime_one)
-        END_TIME = astropy.time.Time(datetime_two)
-
-        info("Successfully parsed cli input!\n")
-
-        return [START_TIME, END_TIME]
-    except Exception as e:
+    except IndexError as e:
         print(str(e))
-        print_help(root_dir)
+        print_help()
+        sys.exit()
+
+    START_TIME = astropy.time.Time(datetime_one)
+    END_TIME = astropy.time.Time(datetime_two)
+
+    return [START_TIME, END_TIME]
 
 
-def print_help(root_dir):
-    with open(f"{Path(root_dir).parent}/README.md", "r") as help_file:
+def print_help():
+    with open(f"{Path(__file__).parent.parent}/README.rst", "r") as help_file:
         for line in help_file.readlines():
             print(line.rstrip())
-    sys.exit()
