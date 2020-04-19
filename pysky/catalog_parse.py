db10359469ee9bc968a8271ad67d593872e790df
allen <rocha021@rangers.uwp.edu>
2020-04-19 14:32:12 -0500

    Parses json files and returns them as dicts

---
+++ pysky/catalog_parse.py
@@ -0,0 +1,38 @@
+"""This module parses the VisibleMessierCatalog.json and
+VisibleCadwellCatalog.json and return them as dictionaries"""
+import json
+import os
+from pathlib import Path
+
+
+def parse_messier(root_dir: str) -> dict:
+    """
+    This function parses VisibleMessierCatalog.json and returns the
+    json object as dictionary
+    :param root_dir: Root directory of this application
+    :return: A Python dictionary of the VisibleMessierCatalog.json
+    """
+    if not isinstance(root_dir, str):
+        root_dir = Path(os.path.dirname(os.path.realpath((__file__))))
+    messier_file = json.loads(open(
+        f"{root_dir}/data/VisibleMessierCatalog.json",
+        "r"
+        ).read())
+    return messier_file
+
+
+def parse_cadwell(root_dir: str) -> dict:
+    """
+    This function parses VisibleCadwellCatalog.json and returns the
+    json object as dictionary
+    :param root_dir: Root directory of this application
+    :return: A Python dictionary of the VisibleCadwellCatalog.json
+    """
+    if not isinstance(root_dir, str):
+        root_dir = Path(os.path.dirname(os.path.realpath((__file__))))
+    cadwell_file = json.loads(open(
+        f"{root_dir}/data/VisibleCadwellCatalog.json",
+        "r"
+        ).read())
+    return cadwell_file
+
