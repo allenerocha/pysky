6412d3c5c1303bf6e3f854b4c6c740d8eb0107ea
allen <rocha021@rangers.uwp.edu>
2020-05-17 15:54:10 -0500

    Added docstring

--- pysky/const.py
+++ pysky/const.py
@@ -1,9 +1,11 @@
 # -*- encoding: utf-8 -*-
+"""Constant variables to be accessed across the application."""
 import os.path
 
 
 class Const(object):
     ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+    SLIDESHOW_DIR = ""
     THREADS = 1
     VERBOSITY = 20
 
