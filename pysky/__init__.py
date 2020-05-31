9f443383bc104709e678648443eb8fbe3558ab01
allen <rocha021@rangers.uwp.edu>
2020-05-31 18:20:38 -0500

    Updated version to v0.9.0

--- pysky/__init__.py
+++ pysky/__init__.py
@@ -5,5 +5,5 @@ __description__ = """This is an application to view what will
                     be visible in the sky in a given a time range."""
 __author__ = """Allen E Rocha"""
 __email__ = 'allenerocha@pm.me'
-__version__ = "0.7.5"
+__version__ = "0.9.0"
 __licence__ = "AGPLv3.0"
