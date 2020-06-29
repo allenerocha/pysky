a34c618d498b95752b99ef9709a704d478739174
allen <rocha021@rangers.uwp.edu>
2020-06-29 09:00:53 -0500

    Updated formatting for the list creation

--- pysky/html_list.py
+++ pysky/html_list.py
@@ -2,14 +2,15 @@
 
 
 class HTML_list:
-    def __init__(self, html_list=None):
+    def __init__(self, html_list=None, delimiter=" "):
+        self.delimiter = delimiter
         if html_list is None:
             self.html_list = list()
         else:
             self.html_list = html_list
 
     def append(self, item) -> None:
-        """Append item(s) to the html list"""
+        """Append item(s) to the html list."""
         self.html_list.append(item)
 
     def __str__(self) -> str:
@@ -17,14 +18,24 @@ class HTML_list:
         for index, item in enumerate(self.html_list):
             if index == 0:
                 out += "<ol>\n"
-
-            out += "<li>"
-
-            for _, value in item.items():
-                out += "<span style='font-weight: 400;'>" + f"{value} " + "</span>"
-            out += "</li>\n"
-            
+            for celestial_obj, propteries in item.items():
+                out += "<li>"
+                out += "<span style='font-weight: 400;'>" +\
+                    f"{celestial_obj.title()}" +\
+                    self.delimiter +\
+                    "</span>"
+                jndex = 0
+                for key, value in propteries.items():
+                    if ' (petameters)' in str(key).lower():
+                        key = str(key).lower().replace(' (petameters)', '').title()
+                        value = '{:,.2f}'.format(value) + ' Pm'
+                    out += "<span style='font-weight: 400;'>" +\
+                        f"{key.title()}: {value}"
+                    if jndex != (len(propteries) - 1):
+                        out += self.delimiter
+                    out += "</span>"
+                    jndex += 1
+                out += "</li>\n"
             if index == (len(self.html_list) - 1):
                 out += "</ol>\n"
-                
         return out
